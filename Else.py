ecc258212a48eb3b878735b95091b05c1393f517
Aday21 <atragar051@g.educaand.es>
2025-09-30 13:49:34 +0200

    Primer commit

---
+++ Else.py
@@ -0,0 +1,9 @@
+num1 = input("Introduce un número: ")
+num2 = input("Introduce otro número: ")
+
+if num1 > num2:
+    print(num1 +" es más grande que "+ num2)
+elif num2 > num1:
+    print(num2 +" es más grande que "+ num1)
+elif num1 == num2:
+    print(num1, "es igual a "+ num2)
---
+++ if.py
@@ -0,0 +1,6 @@
+usuario = input("Hola, escribe tu usuario; ")
+if usuario == "Aday":
+    print("Es correcto", usuario)
+else:
+    print("Usuario incorrecto")
+
