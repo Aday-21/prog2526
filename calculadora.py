d2f4b8161a636e78f318ebd40125312aa8448447
Aday-21 <atragar051@g.educaand.es>
2025-10-01 12:41:42 +0200

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,19 @@
+x = int(input("Introduce un valor; "))
+y = int(input("Introduce un valor; "))
+op = input("Introduce la operación que quieras hacer; ")
+
+if op == "Suma":
+    suma = x + y
+    print("El resultado es; ", suma)
+
+elif op == "Resta":
+    resta = x - y
+    print("El resultado es; ", resta)
+
+elif op == "Multiplicación":
+    Multiplicación = x * y
+    print("El resultado es; ", Multiplicación)
+
+elif op == "División":
+    división = x / y
+    print("El resultado es; ", división)
