ecd781ea69b35adaaa5bd10f5fc0e1c3bb996805
Aday21 <atragar051@g.educaand.es>
2025-10-16 09:23:11 +0200

    nuevas tareas

---
+++ ej35 contraseñas.py
@@ -0,0 +1,10 @@
+nombre = input("Introduzca un nombre; ")
+clave = input("Introduzca una clave; ")
+while len(clave) < 8:
+    clave = input("Introduzca una clave; ")
+print("¡Bienvenido", nombre+"!")
+    
+       
+
+
+    
