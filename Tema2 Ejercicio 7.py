efdcaefc7d7dcf76ca2fb70583d90bcc7503bb8f
Aday-21 <atragar051@g.educaand.es>
2025-11-25 17:56:42 +0100

    Update Tema2 Ejercicio 7.py
    CORRECCIÓN

--- Tema2 Ejercicio 7.py
+++ Tema2 Ejercicio 7.py
@@ -1,11 +1,15 @@
-# definimos la funcion 
-def maximo(t):
-    for e in t:
-        #TODO:  comparar e con 
-        
-return
-    
-    
-a = [5,3,2,8,1]    
+def maximo (t):
+    #Inicializar mayor
+    valor_maximo = t[0]
+    for i in range(len(t)):
+        #comparar e con
+        if t[i] > valor_maximo:
+            valor_maximo = t[i]
+    return valor_maximo        
+
+a = [-11,5,3,2,8,1,2,1,10,2,1,2,1,2,1,2,1]
+# invocar a la función
+num_mayor = maximo(a)
+print(num_mayor)   
 
 
