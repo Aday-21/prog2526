cce23b69ba70f7d3c6e62149d5f2c2addb9a0666
Aday Trandafir <atragar051@g.educaand.es>
2025-12-02 14:50:24 +0100

    Tareas java

--- Ejercicios/src/Ejercicios/Ejercicio4.java
+++ Ejercicios/src/Ejercicios/Ejercicio4.java
@@ -9,5 +9,16 @@ package Ejercicios;
  * @author 09_1DAW_Alum
  */
 public class Ejercicio4 {
-    
+    public static void main(String[] args) {
+        
+        short minimo = (short) -32768;
+        short maximo = (short) 32767;
+        
+        System.out.println("El maximo valor es; "+ maximo);
+        System.out.println("El minimo valor es; "+ minimo);
+        
+        short short_maximo = (short) (maximo + 1);
+        
+        System.out.println(short_maximo);        
+    }
 }
