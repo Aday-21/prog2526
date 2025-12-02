ae8f85c1150091cefbef62280276ff6a5711a872
Aday Trandafir <atragar051@g.educaand.es>
2025-12-02 14:25:17 +0100

    primer commit java

---
+++ Ejercicios/src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 09_1DAW_Alum
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            
+        System.out.println("Introduce un numero; ");
+        Scanner sc = new Scanner(System.in);
+        double numero = sc.nextDouble();
+        System.out.println("El numero es; "+ numero);
+    }
+    
+}
